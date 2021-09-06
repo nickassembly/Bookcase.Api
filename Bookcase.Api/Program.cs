bc37138970d1eab1e6665d87ef965df12e5f04c1
nickguerra <nickg@maliciousgames.com>
2021-09-06 08:46:26 -0500

    Added cors

--- Bookcase.Api/Program.cs
+++ Bookcase.Api/Program.cs
@@ -1,13 +1,28 @@
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add any services
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+// Anything after build is the callback
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/shelf", () => new[] { "/works/OL64468W", "/works/OL103134W" });
+app.UseCors(cfg => 
+{
+ if (app.Environment.IsDevelopment())
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyMethod();
+        cfg.AllowAnyHeader();
+    }
+});
+
+app.MapGet("/shelf", () => new[] { "OL64468W", "OL103134W" });
 
 app.Run();
 
